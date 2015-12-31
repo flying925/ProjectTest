48fc0dc70cea04c354453a40faac976bacb2202a
flying925 <weikejun0925@163.com>
2015-12-31 16:57:19 +0800

    Initial Commit

---
+++ gitTest3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitTest3
+//
+//  Created by weikejun on 15/12/31.
+//  Copyright (c) 2015年 com.qianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitTest3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitTest3
+//
+//  Created by weikejun on 15/12/31.
+//  Copyright (c) 2015年 com.qianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
